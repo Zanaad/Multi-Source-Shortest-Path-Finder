2ea3784fbc258f959640b24d1023e194a008c611
Yllka5 <yllka.kastrati1@student.uni-pr.edu>
2024-12-13 20:32:05 +0100

    Implement singleton in Graph class

--- src/main/java/models/Graph.java
+++ src/main/java/models/Graph.java
@@ -1,24 +1,37 @@
 package models;
 
+import utils.Alerts;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Graph {
-    private int vertices;
+    private int numVertices;
     private List<Edge> edges;
     private int[][] adjacencyMatrix;
 
-    public Graph(int vertices) {
-        this.vertices = vertices;
-        this.edges = new ArrayList<>();
-        this.adjacencyMatrix = new int[vertices][vertices];
+    private static Graph instance;
 
-        for (int i = 0; i < vertices; i++) {
+    public Graph(int numVertices) {
+        this.numVertices = numVertices;
+        this.edges = new ArrayList<>();
+        this.adjacencyMatrix = new int[numVertices][numVertices];
+
+        for (int i = 0; i < numVertices; i++) {
             Arrays.fill(adjacencyMatrix[i], Integer.MAX_VALUE / 2);
             adjacencyMatrix[i][i] = 0;
         }
+        instance = this;
     }
+
+    public static Graph getInstance() {
+        if (instance == null) {
+            Alerts.errorMessage("Graph instance not initialized. Call getInstance(int numVertices) first.");
+        }
+        return instance;
+    }
+
 
     public void addEdge(int source, int destination, int weight) {
         edges.add(new Edge(source, destination, weight));
@@ -29,11 +42,19 @@ public class Graph {
         return adjacencyMatrix;
     }
 
+    public void setAdjacencyMatrix(int[][] adjacencyMatrix) {
+        this.adjacencyMatrix = adjacencyMatrix;
+    }
+
     public List<Edge> getEdges() {
         return edges;
     }
 
     public int getVertices() {
-        return vertices;
+        return numVertices;
+    }
+
+    public void setVertices(int numVertices) {
+        this.numVertices = numVertices;
     }
 }
