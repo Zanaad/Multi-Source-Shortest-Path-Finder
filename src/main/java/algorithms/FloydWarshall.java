f343f080098cf2f26f885e2c0c6404b71f5f6a0d
Zana Ademi <zana.ademi@student.uni-pr.edu>
2024-12-13 17:07:31 +0100

    findShortestPathStepByStep method for FloydWarshall

--- src/main/java/algorithms/FloydWarshall.java
+++ src/main/java/algorithms/FloydWarshall.java
@@ -4,17 +4,47 @@ import models.Graph;
 
 public class FloydWarshall {
     public static int[][] findShortestPaths(Graph graph) {
-        int vertices = graph.getVertices();
+        int numVertices = graph.getVertices();
         int[][] dist = graph.getAdjacencyMatrix();
 
-        for (int k = 0; k < vertices; k++) {
-            for (int i = 0; i < vertices; i++) {
-                for (int j = 0; j < vertices; j++) {
+        for (int k = 0; k < numVertices; k++) {
+            for (int i = 0; i < numVertices; i++) {
+                for (int j = 0; j < numVertices; j++) {
                     dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
 
         return dist;
+    }
+
+    public static int[][][] findShortestPathsStepByStep(Graph graph) {
+        int numVertices = graph.getVertices();
+        int[][] dist = graph.getAdjacencyMatrix();
+        int[][][] steps = new int[numVertices + 1][numVertices][numVertices];
+
+        // Initialize step 0 with the original adjacency matrix
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                steps[0][i][j] = dist[i][j];
+            }
+        }
+
+        for (int k = 0; k < numVertices; k++) {
+            for (int i = 0; i < numVertices; i++) {
+                for (int j = 0; j < numVertices; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+
+            // Save the matrix after this step
+            for (int i = 0; i < numVertices; i++) {
+                for (int j = 0; j < numVertices; j++) {
+                    steps[k + 1][i][j] = dist[i][j];
+                }
+            }
+        }
+
+        return steps;
     }
 }
